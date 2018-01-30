0577ec49ea64556478f60dcb5e4076183929aaab
Syl <s.alonsious@web-siouss.be>
2018-01-30 16:04:59 +0100

    Ajout Sass

--- views/gallerie.php
+++ views/gallerie.php
@@ -1,8 +1,9 @@
-
-<img src="../photos/_IGP5079_1.jpg"/>
-<img src="../photos/_IGP5080_1.jpg"/>
-<img src="../photos/_IGP5128_1.jpg"/>
-<img src="../photos/_IGP5144_1.jpg"/>
-<img src="../photos/_IGP5152_1.jpg"/>
-<img src="../photos/_IGP5168_1.jpg"/>
-<img src="../photos/_IGP5171_1.jpg"/>
+<div class="gallery">
+<img src="../assets/photos/_IGP5079_1.jpg"/>
+<img src="../assets/photos/_IGP5080_1.jpg"/>
+<img src="../assets/photos/_IGP5128_1.jpg"/>
+<img src="../assets/photos/_IGP5144_1.jpg"/>
+<img src="../assets/photos/_IGP5152_1.jpg"/>
+<img src="../assets/photos/_IGP5168_1.jpg"/>
+<img src="../assets/photos/_IGP5171_1.jpg"/>
+</div>

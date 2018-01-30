ddaa4be11bb41b14898efa6f2fc1737bf958a176
Syl <s.alonsious@web-siouss.be>
2018-01-30 15:48:14 +0100

    Ajout contenu gallery

--- views/gallerie.php
+++ views/gallerie.php
@@ -1,1 +1,8 @@
-<h1>GALERIE</h1>
+ 
+<img src="../photos/_IGP5079_1.jpg"/>
+<img src="../photos/_IGP5080_1.jpg"/>
+<img src="../photos/_IGP5128_1.jpg"/>
+<img src="../photos/_IGP5144_1.jpg"/>
+<img src="../photos/_IGP5152_1.jpg"/>
+<img src="../photos/_IGP5168_1.jpg"/>
+<img src="../photos/_IGP5171_1.jpg"/>

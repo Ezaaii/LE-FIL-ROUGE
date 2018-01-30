5d611609f731965044aef31f276e079890b55126
Belpois Nicolas <nicolasbelpois@gmail.com>
2018-01-30 15:09:21 +0100

    ajout de l'html acceuil.php + photos

---
+++ views/acceuil.php
@@ -0,0 +1,8 @@
+<h1>MAZY CULTURE</h1>
+
+<p>Mazy culuture, c'est tout d'abord une salle à loué, située à Mazy à environ 15Km de Namur, 9Km de Gambloux et 3Km de la E42.
+Pour un anniverssaire, une reunion, un repas de famille ou barbecue. Pour organiser un concert, un spectacle ou une exposition.
+Notre salle acceuille jusqu'à 120 personnes. De plus nous offrons un espace exterieur trés agréable par beau temps.</p>
+
+<p>Mazy culture, c'est ensuite une association qui propose des partenariats pour le developpement de projets socio-culturels. Une residence pour crée et construire ces projets.
+Mazy culture c'est aussi un organisateur d'evenements, Concerts, spectacle de magie ect...</p>
